d2e80e585e124635c0751c069cf80ef1f7b03523
shahaf <shahaf@minute.ly>
2018-01-17 13:06:05 +0200

    minor change in cron

--- cron/ids_stream_watch_dog.sh
+++ cron/ids_stream_watch_dog.sh
@@ -5,10 +5,11 @@ PIDFILE="/home/ir/sameacs/tweets-cluster/pids/ids_stream.pid"
 
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
-  echo "Already running."
+  echo "Stream by ids Already running."
   exit 99
 fi
 
+echo "Starting Stream by ids..."
 python /home/ir/sameacs/tweets-cluster/stream_by_ids.py >> /home/ir/sameacs/tweets-cluster/logs/ids_stream.log 2>&1 &
 
 echo $! > "${PIDFILE}"
--- cron/live_stream_watch_dog.sh
+++ cron/live_stream_watch_dog.sh
@@ -5,10 +5,11 @@ PIDFILE="/home/ir/sameacs/tweets-cluster/pids/live_stream.pid"
 
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
-  echo "Already running."
+  echo "Live Stream Already running."
   exit 99
 fi
 
+echo "Starting Live Stream..."
 python /home/ir/sameacs/tweets-cluster/live_stream.py >> /home/ir/sameacs/tweets-cluster/logs/live_stream.log 2>&1 &
 
 echo $! > "${PIDFILE}"
