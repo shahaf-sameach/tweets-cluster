d54d114c14e60bdc847bf88476d9f6b3f2f33d32
shahaf <shahaf@minute.ly>
2018-04-18 19:14:48 +0300

    set datetime of tweet in the database

--- cron/cron.sh
+++ cron/cron.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-while true; do
+while true; 
+do
   sh /home/ir/sameacs/tweets-cluster/cron/live_stream_watchdog.sh >> /home/ir/sameacs/tweets-cluster/log/watchdog.log 2>&1
   sh /home/ir/sameacs/tweets-cluster/cron/ids_stream_watchdog.sh >> /home/ir/sameacs/tweets-cluster/log/watchdog.log 2>&1
   sleep 1m
+done
--- database/database_handler.py
+++ database/database_handler.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from settings import MongoSettings
 from model.tweet import Tweet
+from dateutil.parser import parse
 
 class Database(object):
 
@@ -11,10 +12,10 @@ class Database(object):
     #self.db.authenticate(MongoSettings.user, MongoSettings.pwd)
 
   def get_all_tweets_ids(self):
-    return [t['id'] for t in self.db.tweets.find({}, {'id' : 1})]
+    return [t['_id'] for t in self.db.tweets.find({}, {'_id' : 1})]
 
   def insert_tweet(self, tweet):
-    tweet.update({"_id" : tweet['id']})
+    tweet.update({"_id" : tweet['id'], 'created_at_date' : parse(tweet['created_at'])})
     return self.db.tweets.insert_one(tweet)
 
   def insert_4xx_tweet(self, tweet_id, status, msg):
@@ -24,7 +25,7 @@ class Database(object):
     return [t['_id'] for t in self.db['tweet_4xx'].find({}, {'_id' : 1})]
 
   def find_tweet(self, tweet_id):
-    return self.db.tweets.find({"id" : tweet_id})
+    return self.db.tweets.find({"_id" : tweet_id})
 
   def find_any(self):
     return self.db.tweets.find_one()
@@ -42,9 +43,16 @@ class Database(object):
   def get_random_tweet(self):
     return self.db.tweets.find_one()
 
+  def update_date(self):
+    for t in self.db.tweets.find({ "created_at_date" : { "$exists" : false } }):
+      t['created_at_date'] = parse(t['created_at'])
+      self.db.tweets.save(t)
+
+
 if __name__ == '__main__':
   db = Database()
   t = db.get_random_tweet()
+  print t
   tweet = Tweet(t)
   print tweet.hashtags
   
