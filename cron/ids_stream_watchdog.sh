ddcddff8c704447d84d79163ebd41caee64082f8
shahaf <shahaf@minute.ly>
2018-07-22 18:36:27 +0300

    minor fix

--- cron/ids_stream_watchdog.sh
+++ cron/ids_stream_watchdog.sh
@@ -5,11 +5,11 @@ PIDFILE="/home/ir/sameacs/tweets-cluster/pids/ids_stream.pid"
 
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
-  echo "Stream by ids Already running."
+  echo "$(date '+%d/%m/%Y %H:%M:%S') Stream by ids Already running."
   exit 99
 fi
 
-echo "Starting Stream by ids..."
+echo "$(date '+%d/%m/%Y %H:%M:%S') Starting Stream by ids..."
 source /home/ir/sameacs/.venv/bin/activate
 export PYTHONPATH=/home/ir/sameacs/tweets-cluster
 python /home/ir/sameacs/tweets-cluster/stream/file_stream.py >> /home/ir/sameacs/tweets-cluster/logs/file_stream.log 2>&1 &
--- cron/live_stream_watchdog.sh
+++ cron/live_stream_watchdog.sh
@@ -5,11 +5,11 @@ PIDFILE="/home/ir/sameacs/tweets-cluster/pids/live_stream.pid"
 
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
-  echo "Live Stream Already running."
+  echo "$(date '+%d/%m/%Y %H:%M:%S') Live Stream Already running."
   exit 99
 fi
 
-echo "Starting Live Stream..."
+echo "$(date '+%d/%m/%Y %H:%M:%S') Starting Live Stream..."
 source /home/ir/sameacs/.venv/bin/activate
 export PYTHONPATH=/home/ir/sameacs/tweets-cluster
 python /home/ir/sameacs/tweets-cluster/stream/live_stream.py >> /home/ir/sameacs/tweets-cluster/logs/live_stream.log 2>&1 &
