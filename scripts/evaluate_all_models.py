16f5368b7830101863426658a3cbeadf90b417c7
shahaf <shahaf@minute.ly>
2018-09-07 13:00:12 +0300

    debug line test

--- scripts/evaluate_all_models.py
+++ scripts/evaluate_all_models.py
@@ -36,25 +36,28 @@ file_list = list(map(lambda f: "2017_{}".format(f),
 
 file_list = list(map(lambda f: "2017_{}".format(f), ["03_28", "04_06"]))
 
-tweets = []
+Tweets = []
 golden_standard_clusters = []
+print("loading data from {} files...".format(len(file_list)))
+t0 = time.time()
 for tweet_file in file_list:
-    print("loading {} data...".format(tweet_file))
-    tweets.extend([TweetBuilder(t) for t in get_tweets_from_file("{}.json".format(tweet_file))])
+    print("\tloading {} data...".format(tweet_file))
+    Tweets.extend([TweetBuilder(t) for t in get_tweets_from_file("{}.json".format(tweet_file))])
     golden_standard_clusters.append(map(int, get_ground_truth("{}.json".format(tweet_file))))
 
+print("took {:.3} sec".format(time.time() - t0))
 print("golden standart clusters size: {}".format([len(c) for c in golden_standard_clusters]))
-print("total tweets from files: {}".format(len(tweets)))
+print("total tweets from files: {}".format(len(Tweets)))
 
 golden_standard_ids = set([t_id for cluster in golden_standard_clusters for t_id in cluster])
-tweets_ids = set([t.id for t in tweets])
+tweets_ids = set([t.id for t in Tweets])
 intersection_ids = golden_standard_ids.intersection(tweets_ids)
 print("common ids: {}".format(len(intersection_ids)))
 
 y_true = [[t for t in c if t in intersection_ids] for c in golden_standard_clusters]
 other_ids = tweets_ids.difference(intersection_ids)
 X_tweets_ids = set(np.random.choice(list(other_ids), random_tweets_n, replace=False)).union(intersection_ids)
-X_tweets = [t for t in tweets if t.id in X_tweets_ids]
+X_tweets = [t for t in Tweets if t.id in X_tweets_ids]
 print("running models on {} tweets".format(len(X_tweets)))
 
 
@@ -63,17 +66,17 @@ for model in [TfIdfModel(), Word2VecModel()]:
     print("building {} model...".format(model.__class__.__name__))
     t0 = time.time()
     X = model.build(X_tweets)
-    print("took {} sec".format(time.time() - t0))
-    for alg in (km, af, db, ag):
+    print("took {:.3} sec".format(time.time() - t0))
+    for alg in [km, af, db, ag]:
         print("\tfitting {} ...".format(alg.__class__.__name__))
         t1 = time.time()
         if alg != km:
-            X = distance.pdist(X)
+            X = distance.pdist(X.A)
         model_fit = alg.fit(X)
         name = "{}_{}".format(model_fit.__class__.__name__ ,model.__class__.__name__)
         models.append({"name" : name, "fit" : model_fit})
-        print("\ttook {} sec".format(time.time() - t1))
-    print("took {} sec".format(time.time() - t0))
+        print("\ttook {:.3} sec".format(time.time() - t1))
+    print("took {:.3} sec".format(time.time() - t0))
     print("\n")
 
 
@@ -81,15 +84,15 @@ for symm in [word_sym, sentence_sym, ish_sym]:
     print("building SentenceSymModel model with {} ...".format(symm.__name__))
     t0 = time.time()
     X = SentenceSymModel().build(tweets=X_tweets, method=symm)
-    print("took {} sec".format(time.time() - t0))
+    print("took {:.3} sec".format(time.time() - t0))
     for alg in (af, db, ag):
         print("\tfitting {} ...".format(alg.__class__.__name__))
         t1 = time.time()
         model_fit = alg.fit(X)
         name = "{}_{}_{}".format(model_fit.__class__.__name__, SentenceSymModel().__class__.__name__, symm.__name__)
         models.append({"name": name, "fit": model_fit})
-        print("\ttook {} sec".format(time.time() - t1))
-    print("took {} sec".format(time.time() - t0))
+        print("\ttook {:.3} sec".format(time.time() - t1))
+    print("took {:3.} sec".format(time.time() - t0))
     print("\n")
 
 
@@ -111,9 +114,10 @@ for i, model in enumerate(models):
 
     name = "{}_{}".format(model['name'], len(clusters))
     write_clusters_to_files(clusters, header=header)
-    print("took {} sec".format(time.time() - t0))
+    print("took {:.3} sec".format(time.time() - t0))
     print("\n")
 
+print("took total of {:.3} sec".format(time.time() - t0))
 print("done")
 
 
