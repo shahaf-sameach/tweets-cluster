778d07b2b77224b047c87e4595a1275e7b49920e
shahaf <shahaf@minute.ly>
2018-09-14 08:38:26 +0300

    added logging for test

--- clustering/models/sentence_sym_model.py
+++ clustering/models/sentence_sym_model.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 
 from clustering.similarity.word_sym import symmetric_sentence_similarity
@@ -11,6 +13,7 @@ class SentenceSymModel(object):
 
     def build(self, tweets, method=symmetric_sentence_similarity):
         """ returns sym matrix based sym method"""
+        print("retrive sentences from {} tweets".format(len(tweets)))
         sentences = [t.text for t in tweets]
 
         data_len = len(sentences)
@@ -18,11 +21,21 @@ class SentenceSymModel(object):
         m = np.zeros((data_len, data_len))
         np.fill_diagonal(m, 1.0)
 
+        total_count, count = 1,1
         for i in xrange(data_len):
             for j in xrange(i + 1, data_len):
+                total_count += 1
+
+        print("building simm matrix")
+        for i in xrange(data_len):
+            for j in xrange(i + 1, data_len):
+                t0 = time.time()
+                print("calc sim {}/{}".format(count, total_count))
                 sim = method(sentences[i], sentences[j])
                 m[i][j] = sim
                 m[j][i] = sim
+                count += 1
+                print("took {:.3f}".format(time.time() - t0))
 
         return m
 
